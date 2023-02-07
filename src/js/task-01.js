e6882552350dfdce99a66229afd78c596e99bf1a
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-02-07 22:09:12 +0200

    task 2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -4,5 +4,5 @@ console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach((item) => {
     console.log(`Category: ${item.firstElementChild.textContent}`);
-    console.log(`Elements: ${item.lastElementChild.children.length}`)
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+
+const elements = ingredients.map(option => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = option;
+
+
+  return itemEl;
+});
+console.log(elements)
+
+ingredientsEl.append(...elements);
+
+

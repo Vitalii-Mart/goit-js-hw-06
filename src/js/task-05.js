8775dd71cf0683a9ac7bd1627d89ff5ca21d6708
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-02-08 20:45:59 +0200

    Update task-05.js

---
+++ src/js/task-05.js
@@ -0,0 +1,15 @@
+const inputEl = document.querySelector('#name-input');
+const outputEl = document.querySelector('#name-output');
+
+
+
+inputEl.addEventListener('input', (event) => {
+    outputEl.textContent = event.currentTarget.value.trim();
+
+if(outputEl.textContent === ""){
+    return outputEl.textContent = 'Anonymous';
+   };
+
+});
+
+

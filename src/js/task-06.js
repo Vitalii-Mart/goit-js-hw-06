15a6e24828bdbc095b9cf7ce8c102345c40efcf1
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-02-08 22:00:48 +0200

    Update task-06.js

---
+++ src/js/task-06.js
@@ -0,0 +1,19 @@
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', e => {
+
+
+if(e.currentTarget.value.length === Number(inputEl.getAttribute("data-length"))){
+    inputEl.classList.remove("invalid");
+    return inputEl.classList.add("valid");
+}
+return inputEl.classList.add("invalid");
+});
+
+
+
+
+
+
+
+

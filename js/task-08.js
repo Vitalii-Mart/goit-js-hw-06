ffc45904d76ca1a2aeab286cee15b0a4f9f4964d
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-02-09 22:48:01 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -6,14 +6,25 @@ formEl.addEventListener("submit", (e) => {
     elements: { email, password },
   } = e.currentTarget;
   console.log(`Email: ${email.value}, Password: ${password.value}`);
- 
+
+  const valueImput = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(valueImput);
+
+  const formData = new FormData(e.currentTarget);
+
+  formData.forEach((value, name) => {
+    console.log("name", name);
+    console.log("value", value);
+  });
+
   if (email.value === `` || password.value === ``) {
     alert("Всі поля повинні бути заповнені!");
-  }
-  else{
-    alert("Ласкаво просимо!")
+  } else {
+    alert("Ласкаво просимо!");
   }
   e.currentTarget.reset();
-  
-  
 });

107826d2da0652a144d4c59c518dce620d167178
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-02-09 12:48:07 +0200

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector('.login-form')
 
+
+formEl.addEventListener("submit", e => {
+  e.preventDefault();
+  const {
+    elements: { email, password }
+  } = e.currentTarget;
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+  
+  if (email.value === `` || password.value === ``){
+    alert('Всі поля повинні бути заповнені!')
+  }
+  e.currentTarget.reset();
+
+});
